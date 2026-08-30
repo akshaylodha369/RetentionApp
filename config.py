db220511a7836ce3a3dbcd9d7ebfe724149012eb
Akshay Lodha <akshaylodha369@gmail.com>
2026-08-30 10:35:10 +0530

    Debug Google Places environment variable

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
 
-GOOGLE_PLACES_API_KEY = os.getenv("AIzaSyB7WpHGa_IvxV4LCbh0K4p0kvDZaUZ3Gjg")
+GOOGLE_PLACES_API_KEY = os.environ.get("GOOGLE_PLACES_API_KEY", "")
 
-if not GOOGLE_PLACES_API_KEY:
-    raise RuntimeError(
-        "GOOGLE_PLACES_API_KEY environment variable is not set"
-    )
+print("CONFIG CHECK - API KEY LOADED:", bool(GOOGLE_PLACES_API_KEY))
+print("CONFIG CHECK - API KEY LENGTH:", len(GOOGLE_PLACES_API_KEY))
