0ba45995600eb41383cc3490773150934aa4a86d
Akshay Lodha <akshaylodha369@gmail.com>
2026-08-30 10:02:09 +0530

    Add Google Places API key

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
 import os
+
 GOOGLE_PLACES_API_KEY = os.getenv("AIzaSyB7WpHGa_IvxV4LCbh0K4p0kvDZaUZ3Gjg")
+
+if not GOOGLE_PLACES_API_KEY:
+    raise RuntimeError(
+        "GOOGLE_PLACES_API_KEY environment variable is not set"
+    )
